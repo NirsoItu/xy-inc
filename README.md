474657433b6156cdda491c580d817bac09be67d0
NirsoItu <48810680+NirsoItu@users.noreply.github.com>
2020-02-12 01:02:29 -0200

    Projeto finalizado, sem testes

--- README.md
+++ README.md
@@ -9,13 +9,5 @@ API de serviços de localização de ponto de interesse (POIs).
 
 - Caso ocorra problema de conexão, verifique o arquivo no "application.properies" se as propriedades correspondem as configurações de seu banco de dados como localhost, username e password;
 
-- Serviço de cadastro de POI: através de um client HTTP utilizando o método Post na seguinte url: localhost:8080/pois/addPoi, inserir os valores nos parametros Key: name, cooX1 e cooY1.
-
-- Serviço para listar todos os POIs cadastrados: através de um client HTTP utilizando o método Get na seguinte url: localhost:8080/pois/allPois.
-
-- Serviço para listar POIs por proximidade: através de um client HTTP utilizando o método Get na seguinte url: localhost:8080/pois/underTen.
-
-- Não foi possível completar os testes devido a problemas de configuração do Gradle em minha maquina, mas os métodos estão no arquivo PoisControllerIntegrationTest.java
-
 
 
--- src/main/java/com/xyinc/gps/controller/PoisController.java
+++ src/main/java/com/xyinc/gps/controller/PoisController.java
@@ -3,24 +3,22 @@ package com.xyinc.gps.controller;
 import com.xyinc.gps.model.Pois;
 import com.xyinc.gps.repository.PoisRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
-import java.util.List;
-
 
 @Controller
 @RequestMapping("/pois")
 public class PoisController {
+
     @Autowired
 
     private PoisRepository poisRepository;
-    private Pois pois;
+
 
     //Create a new POI
-    @PostMapping(path = "/addPoi") // Map ONLY POST Requests
+    @PostMapping(path = "/addPoi")
     public @ResponseBody
     String addNewPoi(@RequestParam @Valid String name, @RequestParam @Valid Integer cooY1,
                      @RequestParam @Valid Integer cooX1) {
@@ -31,6 +29,7 @@ public class PoisController {
         n.setCooY1(cooY1);
         poisRepository.save(n);
         return "Poi created wiht success!";
+        //return new ResponseEntity<Pois>(pois, HttpStatus.CREATED); ** Test
     }
 
     //Show all POIs
@@ -40,14 +39,11 @@ public class PoisController {
         return poisRepository.findAll();
     }
 
+
     @GetMapping("/underTen")
     public @ResponseBody
     Iterable<Pois> findByCoo(@RequestParam Integer cooX2, @RequestParam Integer cooY2) {
         return poisRepository.findByCoo(cooX2, cooY2);
 
     }
-
-
-
-
 }
